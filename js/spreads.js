5a6a59bad28ad7faca41e87a9d771957c2285c7c
Ayana Reddick <ayanared@gmail.com>
2020-02-12 18:21:39 -0500

    Add verbage for each query

---
+++ js/spreads.js
@@ -0,0 +1,47 @@
+const spreads = {
+  past_present_future: {
+    name_of_spread: "the past, present, and future spread",
+    card_1_name: "the past",
+    card_1_definition: "the past influences that affect your query",
+    card_2_name: "the present",
+    card_2_definition: "current influences that affect your query",
+    card_3_name: "the future",
+    card_3_definition: "gain more insight on where your current path is taking you"
+  },
+  physical_emotional_spiritual: {
+    name_of_spread: "the physical state, emotional state, and spiritual state",
+    card_1_name: "your physical state",
+    card_1_definition: "your physical state and and how it affects your query",
+    card_2_name: "your emotional state",
+    card_2_definition: "your emotional state and how it affects your query",
+    card_3_name: "your spiritual state",
+    card_3_definition: "gain more insight on your spiritual state and how it affects your query"
+  },
+  think_feel_do:{
+    name_of_spread: "the what I think, what I feel, and what I do spread",
+    card_1_name: "what you think",
+    card_1_definition: "your current thinking as it relates to your query",
+    card_2_name: "what you feel",
+    card_2_definition: "your current feelings and attitudes around your query",
+    card_3_name: "what you do",
+    card_3_definition: "gain more insight on your current actions around your query"
+  },
+  strength_weakness_advice:{
+    name_of_spread: "the strength, weakness, and advice spread",
+    card_1_name: "your strengths",
+    card_1_definition: "your strengths with respect to your query",
+    card_2_name: "your weaknesses",
+    card_2_definition: "areas that you may need support in regards to your query",
+    card_3_name: "advice",
+    card_3_definition: "get advice on what you should keep in mind when making a decision about your query"
+  },
+  op1_op2_choose: {
+    name_of_spread: "the option 1, option 2, and how to choose spread",
+    card_1_name: "option 1",
+    card_1_definition: "the first option of your query",
+    card_2_name: "option 2",
+    card_2_definition: "the second option of your query",
+    card_3_name: "advice",
+    card_3_definition: "get advice on what you should keep in mind when making a decision about both options"
+  },
+}
