bae42454348d81a647ec9c61b72030d8a105b2ac
Ayana Reddick <ayanared@gmail.com>
2020-02-21 14:55:27 -0500

    Use smaller image

--- js/main.js
+++ js/main.js
@@ -13,9 +13,11 @@ const flipCard = function (cardPosition) {
 
 const createBoard = function (num_of_cards) {
   for (let i = 0; i < num_of_cards; i++) {
-    const cardElement = $(`<div class='img-div'><img src='images/back_of_card.jpg'
-                                id='card-pos-${i}'
-                                data-id=${i}></img></div>`);
+    const cardElement = $(`<div class='img-div'>
+                            <img src='images/back_of_card_small.jpg'
+                              id='card-pos-${i}'
+                              data-id=${i}></img>
+                           </div>`);
     cardElement.one('click', function() {
       flipCard(i)
     });
